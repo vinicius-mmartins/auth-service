63f9213e2923c297bc2c2e0475df7a7b8a166b8a
Vinícius Martins <vinicius.martins@tqi.com.br>
2022-12-13 11:03:17 -0300

    first commit

---
+++ src/test/java/com/github/viniciusmartins/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.viniciusmartins.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
